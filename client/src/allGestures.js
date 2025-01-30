34470e7af1f958a53fa28baa3985075e30404b6a
shekhar-11 <rajshekhar111201@gmail.com>
2025-01-31 00:49:00 +0530

    frontend as  client

--- client/src/allGestures.js
+++ client/src/allGestures.js
@@ -1,17 +1,13 @@
 import * as fp from "fingerpose";
 const fingersList  = [fp.Finger.Thumb,fp.Finger.Index, fp.Finger.Middle, fp.Finger.Ring, fp.Finger.Pinky];
 
-// Define the custom "thumbs down" gesture
+
 export const FistGesture = new fp.GestureDescription('fist');
-// thumbsDownGesture.addCurl(fp.Finger.Thumb, fp.FingerCurl.FullCurlCurl);
-// FistGesture.addDirection(fp.Finger.Thumb, fp.FingerDirection.VerticalDown, 1.0); // Increased confidence
-// FistGesture.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalDownLeft, 0.7); // Increased confidence
-// FistGesture.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalDownRight, 0.7); // Increased confidence
 
 // Curl other fingers fully for "thumbs down" gesture
 for (let finger of fingersList) {
-  FistGesture.addCurl(finger, fp.FingerCurl.FullCurl, 1.0); // Full curl only
-  FistGesture.addCurl(finger, fp.FingerCurl.HalfCurl, 0.9); // Full curl only
+  FistGesture.addCurl(finger, fp.FingerCurl.FullCurl, 1.0); 
+  FistGesture.addCurl(finger, fp.FingerCurl.HalfCurl, 0.9); 
 }
 
 export const LeftGesture = new fp.GestureDescription('left');
@@ -28,11 +24,11 @@ for (let finger of fingersList) {
 RightGesture.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1.0); 
 RightGesture.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalLeft, 1.0);
 
-// Export all the gestures you want to use
+
 export const allGestures = [
   fp.Gestures.VictoryGesture,
   fp.Gestures.ThumbsUpGesture,
-  FistGesture, // Include custom gesture here
+  FistGesture, 
   RightGesture,
   LeftGesture
 ];
